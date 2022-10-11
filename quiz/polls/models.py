c9e75e76d30e0194e31189fe205530f89bf6424a
j-dabling <jgdabble@zoho.com>
2022-10-10 18:25:04 -0600

    Updated models to better suit quiz format.

--- quiz/polls/models.py
+++ quiz/polls/models.py
@@ -1,19 +1,24 @@
-import datetime
 from django.db import models
-from django.utils import timezone
 
+
+# Answers must be written out within 200 characters of text.
+# An answer may be pulled randomly for any question as a false answer.
+class Answer(models.Model):
+    answer_text: = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.answer_text
+
+
+# The question must be phrased and written in less than 200 characters of text.
+# The correct_answer will be pulled along with 3 decoy answers.
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+    correct_answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
+
     def __str__(self):
         return self.question_text
-    def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+    
+    def _get_correct_answer(self):
+        return self.correct_answer
 
-
-class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
-    def __str__(self):
-        return self.choice_text
