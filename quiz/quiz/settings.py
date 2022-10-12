65356ce8d07a49498c42ee837d6e7708aba3abeb
Braiden Sommer <braidensommer@yahoo.com>
2022-10-12 15:06:52 -0600

    updated

--- quiz/quiz/settings.py
+++ quiz/quiz/settings.py
@@ -21,6 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+
 key_path = "key.secret"
 if os.path.isfile(key_path):
     text_file = open(key_path, "r")
